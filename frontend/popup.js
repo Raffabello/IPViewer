63c65300040628b05eceee49f7c6ac29a9bf3afb
Raffabello <that.raffaele95bin@gmail.com>
2025-03-23 04:03:39 +0100

    Adjusted the CSS style and added error message display

--- frontend/popup.js
+++ frontend/popup.js
@@ -12,6 +12,13 @@ chrome.tabs.query({active:true, currentWindow:true}, function(activeTab){
         .then(function(location){
             locViewer.innerHTML = location;
         })
+        .catch(function(error){
+            setTimeout(() => {
+                ipViewer.innerHTML = "N/A";
+                locViewer.innerHTML = "N/A";
+                document.getElementById("error-message-label").innerHTML = "Error: " + error;
+            }, 1000)
+        })
     }else{
         console.log("no tab")
     }
